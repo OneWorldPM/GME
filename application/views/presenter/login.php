c68c2903cf3291c9417e292e9dde4614aadfb142
athulak <athullive@gmail.com>
2021-04-23 23:34:45 +0200

    Logo change

--- application/views/presenter/login.php
+++ application/views/presenter/login.php
@@ -73,7 +73,7 @@
                     <form class="form-login" id="frm_login" name="frm_login" method="post" action="<?= base_url() ?>presenter/login/authentication">
                         <div class="row">
                             <div  style="text-align: center;">                           
-                                <img src="<?= base_url() ?>assets/images/Header_Logos_GME.png" alt="" style="text-align: center;" >
+                                <img src="<?= base_url() ?>front_assets/images/cco-gme-logo.png" alt="" style="text-align: center;" >
                             </div>
                         </div>
                         <fieldset>
