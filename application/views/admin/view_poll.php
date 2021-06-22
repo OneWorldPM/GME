474f3b6c874fb434e330938126df2dedfb2719f0
Rexter Dayuta <rexterdayuta@gmail.com>
2021-06-23 02:11:06 +0800

    Admin poll answer

--- application/views/admin/view_poll.php
+++ application/views/admin/view_poll.php
@@ -39,6 +39,7 @@
                                             <th>Comparison With</th>
                                             <th>Slide Number</th>
                                             <th>Poll Instruction</th>
+                                            <th>Correct Answer</th>
                                             <th>Modify</th>
                                             <th>Action</th>
                                         </tr>
@@ -75,9 +76,14 @@
 
                                                     </form>
                                                         </div>
-                                                    </div> 
-                                                   
-                                                </td>
+                                                    </div>
+                                                    </td>
+
+                                                    <td style="width: 80px">
+                                                        Answer 1: <strong style="font-size: 18px; color: red"><?=$val->correct_answer1?></strong><br>
+                                                        Answer 2: <strong style="font-size: 18px; color: orangered"><?=$val->correct_answer2?></strong>
+                                                    </td>
+
                                                     <td>
                                                         <a class="btn btn-primary btn-sm" href="<?= base_url() . 'admin/sessions/editPollQuestion/' . $val->sessions_poll_question_id ?>">
                                                             <i class="fa fa-pencil"></i> Edit
