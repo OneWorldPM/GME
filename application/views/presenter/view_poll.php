49893f58f75327f3f7c5c3ab82a6628ed64a853d
Rexter Dayuta <rexterdayuta@gmail.com>
2021-06-30 12:46:16 +0800

    Presenter view poll update

--- application/views/presenter/view_poll.php
+++ application/views/presenter/view_poll.php
@@ -21,24 +21,32 @@
                                 <table class="table table-bordered table-striped text-center" id="user">
                                     <thead class="th_center">
                                         <tr>
-                                            <th>Question</th>
-                                            <th>Slide Number</th>
-                                            <th>Instructions</th>
+                                            <th>#</th>
+                                            <th id="quest-id-width">Question ID</th>
+                                            <th id="quest-width">Question</th>
+                                            <th>Poll Name</th>
                                             <th>Poll Type</th>
-                                            <th>Options</th>
-                                            <th>People Voted</th>
+                                            <th>Poll Option</th>
+                                            <th>Total Votes</th>
+                                            <th>Comparison With</th>
+                                            <th>Slide Number</th>
+                                            <th>Poll Instruction</th>
+                                            <th>Correct Answer</th>
+                                            <th>Modify</th>
                                             <th>Action</th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         <?php
                                         if (isset($poll_data) && !empty($poll_data)) {
-                                            foreach ($poll_data as $val) {
+                                            foreach ($poll_data as $index=>$val) {
+//                                                print_r($val);exit;
                                                 ?>
                                                 <tr>
+                                                    <td><?=$index+1?></td>
+                                                    <td><?= $val->sessions_poll_question_id ?></td>
                                                     <td style="text-align:left !important"><?= $val->question ?></td>
-													 <td><?= $val->slide_number ?></td>
-                                                    <td><?=$val->poll_instruction?></td>
+                                                    <td><?=$val->poll_name?></td>
                                                     <td><?= $val->poll_type ?></td>
                                                     <td>
                                                         <?php
@@ -52,7 +60,15 @@
                                                         ?>
                                                     </td>
                                                     <td><?= $val->total_votes ?></td>
-                                                    <td> 
+                                                    <td><?=(isset($val->poll_comparison_id) && ($val->poll_comparison_id)!=='0')?$val->poll_comparison_id:'None' ?></td>
+                                                    <td><?=$val->slide_number?></td>
+                                                    <td><?=$val->poll_instruction?></td>
+                                                    <td style="width: 80px">
+                                                        <?=((!isset($val->correct_answer1) || ($val->correct_answer1 =='0') && (!isset($val->correct_answer1) || $val->correct_answer2 =='0')))?'None Set':''?>
+                                                        <span ><?=(isset($val->correct_answer1) && ($val->correct_answer1 !=='0'))?'Answer 1: <strong style="font-size: 18px; color: red">'.$val->correct_answer1.'</strong>':''?></span><br>
+                                                        <span ><?=(isset($val->correct_answer1) && ($val->correct_answer2 !=='0'))?'Answer 2: <strong style="font-size: 18px; color: red">'.$val->correct_answer2.'</strong>':''?></span>
+                                                    </td>
+                                                    <td>
                                                         <a class="btn btn-primary btn-sm" href="<?= base_url() . 'presenter/sessions/editPollQuestion/' . $val->sessions_poll_question_id ?>">
                                                             <i class="fa fa-pencil"></i> Edit
                                                         </a>
@@ -62,6 +78,9 @@
                                                         <a class="btn btn-danger btn-sm" href="<?= base_url() . 'presenter/sessions/poll_redo/' . $val->sessions_poll_question_id ?>">
                                                             <i class="fa fa-check"></i> Redo
                                                         </a>
+                                                    </td>
+                                                    <td>
+
                                                         <?php if ($val->status == 0) { ?>
                                                             <a href="<?= base_url() ?>presenter/sessions/open_poll/<?= $val->sessions_poll_question_id ?>" class="btn btn-success btn-sm">Open Poll</a>
                                                         <?php } else if ($val->status == 1) { ?>
@@ -70,12 +89,12 @@
                                                            <a href="<?= base_url() ?>presenter/sessions/start_timer/<?= $val->sessions_poll_question_id ?>" class="btn btn-blue btn-sm">Start Timer</a>
                                                         <?php } else if ($val->status == 2) { ?>
                                                             <a href="<?= base_url() ?>presenter/sessions/close_result/<?= $val->sessions_poll_question_id ?>" class="btn btn-warning btn-sm">Close Results</a>
-                                                        <?php } else if ($val->status == 4) { ?>    
+                                                        <?php } else if ($val->status == 4) { ?>
                                                             <a href="<?= base_url() ?>presenter/sessions/show_result/<?= $val->sessions_poll_question_id ?>" class="btn btn-primary btn-sm">Show Results</a>
                                                         <?php } else { ?>
                                                             <a href="<?= base_url() ?>presenter/sessions/show_result/<?= $val->sessions_poll_question_id ?>" class="btn btn-primary btn-sm">Show Results Again</a>
                                                             <a href="<?= base_url() ?>presenter/sessions/view_result/<?= $val->sessions_poll_question_id ?>" class="btn btn-beige btn-sm">View</a>
-                                                        <?php } ?>    
+                                                        <?php } ?>
                                                     </td>
                                                 </tr>
                                                 <?php
